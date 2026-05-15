77df14a45da617127ed1d889320126eaad8982bc
TrooperLooper <lars@recap.dk>
2026-05-15 12:29:58 +0200

    Revert MONTH_NAMES to winter months only (Oct-May)

--- frontend/src/constants/months.ts
+++ frontend/src/constants/months.ts
@@ -10,16 +10,12 @@ export const MONTHS = [
 ];
 
 export const MONTH_NAMES: { [key: number]: string } = {
+  10: "Oktober",
+  11: "November",
+  12: "December",
   1: "Januari",
   2: "Februari",
   3: "Mars",
   4: "April",
   5: "Maj",
-  6: "Juni",
-  7: "Juli",
-  8: "Augusti",
-  9: "September",
-  10: "Oktober",
-  11: "November",
-  12: "December",
 };
